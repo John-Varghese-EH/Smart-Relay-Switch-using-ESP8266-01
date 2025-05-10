2172e73214ac41b6f9f60ad52c2071dc2ca78f89
John Varghese <126874846+John-Varghese-EH@users.noreply.github.com>
2025-05-10 11:38:57 +0530

    thingProperties.h - v2.0

--- SmartSwitch/thingProperties.h
+++ SmartSwitch/thingProperties.h
@@ -1,5 +1,3 @@
-// Arduino IoT Cloud Properties and Connection Handler
-
 #include <ArduinoIoTCloud.h>
 #include <Arduino_ConnectionHandler.h>
 
@@ -9,16 +7,20 @@ const char PASS[]               = ""; // WiFi Password
 const char DEVICE_KEY[]         = ""; // Secret device key
 
 void onSwitch1Change();
+void onMessagesChange();
 
 // Cloud variables
 CloudSwitch switch1;
 CloudContactSensor switchStatus;
+String messages;
 
 void initProperties() {
   ArduinoCloud.setBoardId(DEVICE_LOGIN_NAME);
   ArduinoCloud.setSecretDeviceKey(DEVICE_KEY);
+  
   ArduinoCloud.addProperty(switch1, READWRITE, ON_CHANGE, onSwitch1Change);
   ArduinoCloud.addProperty(switchStatus, READ, ON_CHANGE, NULL);
+  ArduinoCloud.addProperty(messages, READWRITE, ON_CHANGE, onMessagesChange);
 }
 
 WiFiConnectionHandler ArduinoIoTPreferredConnection(SSID, PASS);
